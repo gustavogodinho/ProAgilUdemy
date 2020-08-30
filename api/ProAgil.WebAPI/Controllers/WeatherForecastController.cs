67eef4f496dbcf5d34a3ffcfd8e2ef63ba85ab41
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2020-08-30 11:47:54 -0300

    push API

---
+++ api/ProAgil.WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _datacontext;
+        public WeatherForecastController(DataContext datacontext)
+        {
+            _datacontext = datacontext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var r = await _datacontext.Eventos.ToListAsync(); 
+                return Ok(r);    
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "falhou");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult>  Get(int id)
+        {
+            try
+            {
+                var r = await _datacontext.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(r);    
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "falhou");
+            }
+        }
+    }
+}
---
+++ api/ProAgil.WebAPI/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Model;
+using System.Data;
+
+namespace ProAgil.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Evento> Eventos { get; set; }
+
+    }
+}
+
+
+
