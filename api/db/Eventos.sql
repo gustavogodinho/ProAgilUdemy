a27006fe111f5508e65176d0b434cce26eac2e0f
Gustavo Godinho <gustavogodinho6@msn.com>
2020-09-11 16:25:43 -0300

    Ajuste GlobalJson e Conection String

---
+++ api/db/Eventos.sql
@@ -0,0 +1,12 @@
+ 
+ create table Eventos
+    (
+		 EventoId int,
+         Local varchar(200), 
+         DataEvento varchar(200), 
+         Tema varchar(200),
+		 QtdPessoas int,
+         Lote varchar(200),
+         ImagemURL varchar(200)
+    )
+
