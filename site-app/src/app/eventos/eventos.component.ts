f0e5c80c147455e7aca779923e9a6995dca97f0d
Gustavo Godinho <gustavogodinho6@msn.com>
2020-09-23 01:40:02 -0300

    Add Evento Service

--- site-app/src/app/eventos/eventos.component.ts
+++ site-app/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { EventoService } from '../_services/evento.service';
+import { Evento } from '../_models/Evento';
 
 @Component({
   selector: 'app-eventos',
@@ -18,21 +19,21 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  eventosFiltrados: any = [];
-  eventos: any = [];
+  eventosFiltrados: Evento[];
+  eventos: Evento[];
   imagemAltura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
 
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService ) { }
 
   ngOnInit() {
     this.getEventos();
   }
 
- filtrarEventos(filtrarPor: string): any {
+ filtrarEventos(filtrarPor: string): Evento[] {
   filtrarPor = filtrarPor.toLocaleLowerCase();
   return this.eventos.filter(
     evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
@@ -44,9 +45,10 @@ export class EventosComponent implements OnInit {
   }
 
   getEventos() {
-    this.http.get('http://localhost:5000/values/').subscribe(response => {
-      this.eventos = response;
-      console.log(response);
+    this.eventoService.getAllEvento().subscribe(
+      (eventos: Evento[]) => {
+      this.eventos = eventos;
+      console.log(this.eventos);
     }, error => {
       console.log(error);
     });
