5b3cf32e16183eae52204b8bbc9e7ce46a7c458b
Gustavo Godinho <gustavogodinho6@msn.com>
2020-09-23 19:16:30 -0300

    ngx

--- site-app/src/app/app.module.ts
+++ site-app/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
@@ -18,6 +24,10 @@ import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipePipe.pipe';
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     FormsModule
