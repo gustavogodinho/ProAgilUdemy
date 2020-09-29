50d1991aa4512098c6966ebb8ed32a12f4cc4270
Gustavo Godinho <gustavogodinho6@msn.com>
2020-09-28 23:37:53 -0300

    Angular Rotas alertas ,  ngx toastr

---
+++ site-app/src/app/Contatos/Contatos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos',
+  templateUrl: './Contatos.component.html',
+  styleUrls: ['./Contatos.component.scss']
+})
+export class ContatosComponent implements OnInit {
+
+  titulo = 'Contatos';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
