fc7177a0020ee64b48e53b3eab7d558ec1a35792
Gustavo Godinho <gustavogodinho6@msn.com>
2020-10-03 23:46:00 -0300

    organizaçao do codigo user controller

--- api/ProAgil.WebAPI/Controllers/UserController.cs
+++ api/ProAgil.WebAPI/Controllers/UserController.cs
@@ -17,120 +17,123 @@ using ProAgil.WebAPI.Dtos;
 
 namespace ProAgil.WebAPI.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class UserController : ControllerBase
-  {
-    private readonly IConfiguration _config;
-    private readonly UserManager<User> _userManager;
-    private readonly SignInManager<User> _signInManager;
-    private readonly IMapper _mapper;
-
-    public UserController(IConfiguration config,
-                          UserManager<User> userManager,
-                          SignInManager<User> signInManager,
-                          IMapper mapper)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
-      _signInManager = signInManager;
-      _mapper = mapper;
-      _config = config;
-      _userManager = userManager;
-    }
+        private readonly IConfiguration _config;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
 
-    [HttpGet("GetUser")]
-    public async Task<IActionResult> GetUser()
-    {
-        return Ok(new UserDto());
-    }
-
-    [HttpPost("Register")]
-    [AllowAnonymous]
-    public async Task<IActionResult> Register(UserDto userDto)
-    {
-        try
+        public UserController(IConfiguration config,
+                              UserManager<User> userManager,
+                              SignInManager<User> signInManager,
+                              IMapper mapper)
         {
-            var user = _mapper.Map<User>(userDto);
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _config = config;
+            _userManager = userManager;
+        }
 
-            var result = await _userManager.CreateAsync(user ,userDto.Password);
+        [HttpGet("GetUser")]
+        [AllowAnonymous]
 
-            var userToReturn = _mapper.Map<UserDto>(user);
+        public async Task<IActionResult> GetUser()
+        {
+            return Ok(new UserDto());
+        }
 
-            if (result.Succeeded) 
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            try
             {
-                return Created("GetUser", userToReturn);
+                var user = _mapper.Map<User>(userDto);
+
+                var result = await _userManager.CreateAsync(user, userDto.Password);
+
+                var userToReturn = _mapper.Map<UserDto>(user);
+
+                if (result.Succeeded)
+                {
+                    return Created("GetUser", userToReturn);
+                }
+
+                return BadRequest(result.Errors);
             }
-            
-            return BadRequest(result.Errors);
-        }
-        catch (System.Exception ex)
-        {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco Dados Falhou {ex.Message}");
-        }
-    }
-
-    [HttpPost("Login")]
-    [AllowAnonymous]
-    public async Task<IActionResult> Login(UserLoginDto userLogin)
-    {
-        try
-        {
-            var user = await _userManager.FindByNameAsync(userLogin.UserName);
-
-            var result = await _signInManager.CheckPasswordSignInAsync(user, userLogin.Password, false);
-
-            if (result.Succeeded)
+            catch (System.Exception ex)
             {
-                var appUser = await _userManager.Users
-                    .FirstOrDefaultAsync(u => u.NormalizedUserName == userLogin.UserName.ToUpper());
-
-                var userToReturn = _mapper.Map<UserLoginDto>(appUser);
-
-                return Ok(new {
-                    token = GenerateJWToken(appUser).Result,
-                    user = userToReturn
-                });
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco Dados Falhou {ex.Message}");
             }
-
-            return Unauthorized();
         }
-        catch (System.Exception ex)
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserLoginDto userLogin)
         {
-            return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco Dados Falhou {ex.Message}");
-        }
-    }
+            try
+            {
+                var user = await _userManager.FindByNameAsync(userLogin.UserName);
 
-    private async Task<string> GenerateJWToken(User user)
-    {
-        var claims = new List<Claim>
+                var result = await _signInManager.CheckPasswordSignInAsync(user, userLogin.Password, false);
+
+                if (result.Succeeded)
+                {
+                    var appUser = await _userManager.Users
+                        .FirstOrDefaultAsync(u => u.NormalizedUserName == userLogin.UserName.ToUpper());
+
+                    var userToReturn = _mapper.Map<UserLoginDto>(appUser);
+
+                    return Ok(new
+                    {
+                        token = GenerateJWToken(appUser).Result,
+                        user = userToReturn
+                    });
+                }
+
+                return Unauthorized();
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco Dados Falhou {ex.Message}");
+            }
+        }
+
+        private async Task<string> GenerateJWToken(User user)
+        {
+            var claims = new List<Claim>
         {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
             new Claim(ClaimTypes.Name, user.UserName)
         };
 
-        var roles = await _userManager.GetRolesAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
 
-        foreach (var role in roles)
-        {
-            claims.Add(new Claim(ClaimTypes.Role, role));
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
-
-        var key = new SymmetricSecurityKey(Encoding.ASCII
-            .GetBytes(_config.GetSection("AppSettings:Token").Value));
-
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddDays(1),
-            SigningCredentials = creds
-        };
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-
-        return tokenHandler.WriteToken(token);
     }
-  }
 }
